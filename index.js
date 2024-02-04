c8f9e6ec773bda676d77e8cbdd3a6e514d007176
zainab99a <amanhal729@gmail.com>
2024-02-04 11:38:38 +0300

    Comparison operators

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ var num1=55
 var num2=20
 
 console.log(num1,">",num2 ,":", num1>num2)
+console.log(num1,"<",num2 ,":", num1<num2)
+console.log(num1,">=",num2 ,":", num1>=num2)
+console.log(num1,"<=",num2 ,":", num1<=num2)
+console.log(num1,"==",num2 ,":", num1==num2)
+console.log(num1,"!=",num2 ,":", num1!=num2)
+
